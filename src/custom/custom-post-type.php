ca74a1cecd48ddf60bd11496f3bfee7773aff209
Jeff Cleverley <jeff@jeffcleverley.com>
2017-07-25 23:21:47 +0800

    refactored get_all_post_type_features() - used array_diff to compare  to

--- src/custom/custom-post-type.php
+++ src/custom/custom-post-type.php
@@ -16,15 +16,13 @@ add_action( 'init', __NAMESPACE__ . '\register_custom_post_type' );
 /**
  * Register the custom post type.
  *
- * @since 1.0.0
+ * @since 0.0.1
  *
  * @return void
  */
 function register_custom_post_type() {
 	$labels = array(
-		'name'               => _x( 'Team Bios', 'post type general name', 'teambios' ),
 		'singular_name'      => _x( 'Team Bio', 'post type singular name', 'teambios' ),
-		'menu_name'          => _x( 'Team Bios', 'admin menu', 'teambios' ),
 		'name_admin_bar'     => _x( 'Team Bio', 'add new on admin bar', 'teambios' ),
 		'add_new'            => _x( 'Add New Team Bio', 'team-bios', 'teambios' ),
 		'add_new_item'       => __( 'Add New Team Bio', 'teambios' ),
@@ -34,19 +32,20 @@ function register_custom_post_type() {
 		'all_items'          => __( 'All Team Bios', 'teambios' ),
 		'search_items'       => __( 'Search Team Bios', 'teambios' ),
 		'parent_item_colon'  => __( 'Parent Team Bios:', 'teambios' ),
-		'not_found'          => __( 'No team bios found.', 'teambios' ),
-		'not_found_in_trash' => __( 'No team bios found in Trash.', 'teambios' ),
+		'not_found'          => __( 'No Team Bios found.', 'teambios' ),
+		'not_found_in_trash' => __( 'No Team Bios found in Trash.', 'teambios' ),
 		'featured_image'        => __( 'Profile Image', 'teambios' ),
 		'set_featured_image'    => __( 'Set Profile Image', 'teambios' ),
 		'remove_featured_image' => __( 'Remove Profile Image', 'teambios' ),
 		'use_featured_image'    => __( 'Use Profile Image', 'teambios' ),
 	);
+
 	$features = get_all_post_type_features( 'post', array(
 		'excerpt',
 		'comments',
 		'trackbacks',
-		'custom-fields',
 	) );
+
 	$args = array(
 		'label'        => __( 'Team Bios', 'teambios' ),
 		'labels'       => $labels,
@@ -56,29 +55,29 @@ function register_custom_post_type() {
 		'hierarchical' => false,
 		'has_archive'  => true,
 	);
+
 	register_post_type( 'team-bios', $args );
 }
 /**
  * Get all the post type features for the given post type.
  *
- * @since 1.0.0
+ * @since 0.0.1
  *
  * @param string $post_type Given post type
  * @param array $exclude_features Array of features to exclude
  *
  * @return array
  */
-function get_all_post_type_features( $post_type = 'post', $exclude_features = array() ) {
+function get_all_post_type_features( $post_type = 'post', $excluded_features = array() ) {
+
 	$configured_features = get_all_post_type_supports( $post_type );
-	if ( ! $exclude_features ) {
+
+	if ( ! $excluded_features ) {
 		return array_keys( $configured_features );
 	}
-	$features = array();
-	foreach ( $configured_features as $feature => $value ) {
-		if ( in_array( $feature, $exclude_features ) ) {
-			continue;
-		}
-		$features[] = $feature;
-	}
+
+	$configured_features = array_keys( $configured_features );
+	$features = array_diff( $configured_features, $excluded_features );
+
 	return $features;
 }
